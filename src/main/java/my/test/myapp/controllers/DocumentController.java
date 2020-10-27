cb046c29d55485e358de856e200f881094e0d9fd
Roman Zalevsky <spinnetx@gmail.com>
2020-10-27 12:06:30 +0300

    removed import

--- src/main/java/my/test/myapp/controllers/DocumentController.java
+++ src/main/java/my/test/myapp/controllers/DocumentController.java
@@ -6,7 +6,6 @@ import my.test.myapp.repositories.DocumentRepository;
 import my.test.myapp.services.FileService;
 
 import org.json.JSONObject;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
